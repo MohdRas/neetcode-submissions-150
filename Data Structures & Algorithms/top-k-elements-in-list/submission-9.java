13c9cd47e54c17c2f2e3ccd52dee5f77c4605a3c
Mohd Rasid <32391540+MohdRas@users.noreply.github.com>
2026-08-24 12:43:59 +0530

    Add: top-k-elements-in-list - submission-9

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-9.java
@@ -0,0 +1,105 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        
+        // // convert array into frequency map
+        // Map<Integer, Integer> map = new HashMap<>();
+
+        // for(int i=0; i<nums.length ; i++){
+        //     int freq = 0;
+        //     if(map.containsKey(nums[i])){
+        //         freq = map.get(nums[i]);
+        //     }
+        //     map.put(nums[i], ++freq);
+        // }
+
+        // // convert frequency map to array of lists
+
+        // List[] freqArray = new ArrayList[nums.length];
+
+        // for(Integer key : map.keySet()){
+        //     Integer frequency = map.get(key);
+
+        //     List<Integer> list = new ArrayList<>();
+
+        //     if(freqArray[frequency-1] != null){
+        //         list = freqArray[frequency-1];
+        //     }
+            
+        //     list.add(key);
+        //     freqArray[frequency-1] = list;
+        // }
+
+        // int[] finalResult = new int[k];
+        // int count = 0;
+        // for( int i=freqArray.length-1; i>=0; i--){
+        //    if(freqArray[i] != null){
+        //         for(int j=0; j<freqArray[i].size(); j++){
+        //             finalResult[count++] = (Integer)freqArray[i].get(j);
+        //             if(count==k){
+        //                 return finalResult;
+        //             }
+        //         }
+        //    }
+        // }
+
+        // return finalResult;
+
+        // // iterate array and keep counting until k
+        // // store in array of size k
+        // // return array.
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int n =  nums.length;
+
+        for(int i=0; i<n; i++){
+
+            if(map.containsKey(nums[i])){
+                map.put(nums[i], map.get(nums[i])+1);
+            }else{
+                map.put(nums[i], 1);
+            }
+
+        }
+
+        List[] frequencyArray =  new List[n+1];
+
+        for(Map.Entry entry: map.entrySet()){
+
+            int key = (int)entry.getKey();
+            int frequency = (int)entry.getValue();
+
+            List<Integer> freqList = new ArrayList<>();
+
+            if(frequencyArray[frequency] !=null){
+               freqList = frequencyArray[frequency]; 
+            }
+
+            freqList.add(key);
+            frequencyArray[frequency] = freqList;
+
+        }
+
+        int[] finalArray = new int[k];
+
+        int count = 0;
+
+        for(int i=n; i>0; i--){
+            List<Integer> freqList = frequencyArray[i];
+
+            if(freqList != null){
+                for(int j=0; j<freqList.size(); j++){
+                    if(count < k){
+                        finalArray[count++] = freqList.get(j);
+                    }
+            }
+            }
+            
+        }
+
+        return finalArray;
+
+
+
+    }
+}
