c6c5493a39d487bbdb8f560b453055e0c4399c01
Mohd Rasid <32391540+MohdRas@users.noreply.github.com>
2026-09-01 00:04:06 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,55 @@
+class MinStack {
+
+    Stack<Integer> values;
+    Stack<Integer> mins;
+
+    public MinStack() {
+        // values = push input values as they are coming
+        values = new Stack<>();
+
+        // find min of "min of previous values" and "current value"
+        mins = new Stack<>();
+    }
+    
+    public void push(int val) {
+
+        // first value
+        if(values.isEmpty()){
+            values.push(val);
+            mins.push(val);
+        }else{
+            values.push(val);
+
+            // "min of previous values"
+            int previousMin = mins.peek();
+            // find min of "min of previous values" and "current value"
+            mins.push(Math.min(val, previousMin)); 
+        }
+        
+    }
+    
+    public void pop() {
+        // pop only if not empty
+        
+        if(!values.isEmpty()){
+            values.pop();
+        }
+        if(!mins.isEmpty()){
+            mins.pop();
+        }
+    }
+    
+    public int top() {
+        // no need of empty check
+        
+        // top only from "values" stack
+        return values.peek();    
+    }
+    
+    public int getMin() {
+        // no need of empty check
+        
+        // min only from "mins" stack
+        return mins.peek();
+    }
+}
