208a78bb1888b3aa7987d9aac13f0327971cda55
Mohd Rasid <32391540+MohdRas@users.noreply.github.com>
2026-09-07 13:17:44 +0530

    Add: time-based-key-value-store - submission-6

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-6.java
@@ -0,0 +1,52 @@
+class TimeMap {
+
+    private Map<String, List<TimeMapValue>> timemap;
+
+    class TimeMapValue{
+        public String value;
+        public int timestamp;
+
+        TimeMapValue(String value, int timestamp){
+            this.value = value;
+            this.timestamp =  timestamp;
+        }
+
+    }
+
+    public TimeMap() {
+        timemap = new HashMap<>();  
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        timemap.putIfAbsent(key, new ArrayList<>()); // create a new entry
+        timemap.get(key).add(new TimeMapValue(value,timestamp)); // update existing entry
+    }
+    
+    public String get(String key, int timestamp) {
+
+        List<TimeMapValue> valueTimeList = timemap.get(key);
+
+        if(valueTimeList==null) return "";
+
+        int start = 0;
+        int end = valueTimeList.size() - 1;
+
+        String largestValue = "";
+       
+
+        while(start <= end){
+            
+            int mid = start + ( end - start + 1)/2;
+
+            TimeMapValue midValueTime = valueTimeList.get(mid);
+
+            if(midValueTime.timestamp <= timestamp){
+                largestValue = midValueTime.value;
+                start = mid+1;
+            }else if(midValueTime.timestamp > timestamp){
+                end = mid - 1;
+            }
+        }
+        return largestValue;
+    }   
+}
