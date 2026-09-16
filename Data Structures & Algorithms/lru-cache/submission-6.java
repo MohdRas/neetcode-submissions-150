a7396b75c6a76237575d361702cf461ab4c333c1
Mohd Rasid <32391540+MohdRas@users.noreply.github.com>
2026-09-16 16:37:46 +0530

    Add: lru-cache - submission-6

---
+++ Data Structures & Algorithms/lru-cache/submission-6.java
@@ -0,0 +1,90 @@
+class LRUCache {
+
+    class Node{
+        int key;
+        int val;
+        Node next;
+        Node prev;
+
+        Node(int key, int val){
+            this.key = key;
+            this.val = val; 
+        }
+    }
+
+    Map<Integer, Node> map;
+    Node head;
+    Node tail;
+    int capacity;
+
+    public LRUCache(int capacity) {
+
+        // store capacity
+        this.capacity = capacity;
+        this.map = new HashMap<>(capacity);
+
+        // dummy "head" and "tail"
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+
+        // link "head" and "tail"
+        head.next = tail;
+        tail.prev = head;  
+    }
+    
+    public int get(int key) {
+        if(!map.containsKey(key)) return -1;
+
+        Node node = map.get(key);
+
+        // re-position
+        remove(node);
+        insertAtHead(node);
+        return node.val;
+        
+    }
+    
+    public void put(int key, int value) {
+
+        if(map.containsKey(key)){
+
+            Node node = map.get(key);
+            node.val = value; // update value
+            
+            // re-position
+            remove(node);
+            insertAtHead(node);
+
+        }else{
+            if(map.size() == capacity){
+                Node lru = tail.prev;
+                remove(lru); // remove LRU from DDL
+                map.remove(lru.key); // remove LRU from map via key
+            }
+            Node node = new Node(key,value);
+            insertAtHead(node); // insert new node at head
+            map.put(key, node); // put new node in map
+        }
+        
+    }
+
+    void remove(Node node){
+        // node.prev  <===> node   <====> node.next
+
+        // bypassing the middle man
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+
+    }
+    void insertAtHead(Node node){
+        //head <=====> head.next
+
+        // new node looks around "first"
+        node.next = head.next; 
+        node.prev = head;      
+
+        // neighbors looks at the new node "last"
+        head.next.prev = node; 
+        head.next = node;      
+    }
+}
